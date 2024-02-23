5b8311a2a15b318734d305e3fa3bceeb516cf3c2
Majid <majid.mjh@gmail.com>
2024-02-23 01:17:46 +0100

    add word just for functionality test

---
+++ src/word/word.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { CreateWordDto } from "./dto/create-word.dto";
+import { UpdateWordDto } from "./dto/update-word.dto";
+
+@Injectable()
+export class WordService {
+  private users: UpdateWordDto[] = [];
+
+  create(createWordDto: CreateWordDto): number {
+    const id = this.users.length + 1;
+    this.users.push({
+      id: id,
+      text: createWordDto.text,
+    } as UpdateWordDto);
+    return id;
+  }
+
+  findAll(): UpdateWordDto[] {
+    return this.users;
+  }
+
+  findOne(id: number): UpdateWordDto {
+    return this.users.find((t) => t.id === id);
+  }
+
+  update(id: number, updateWordDto: UpdateWordDto): void {
+    const user = this.users.find((t) => t.id === id);
+    this.users[this.users.indexOf(user)].text = updateWordDto.text;
+  }
+
+  remove(id: number): void {
+    this.users = this.users.filter((t) => t.id !== id);
+  }
+}
