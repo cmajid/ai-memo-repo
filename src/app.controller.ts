4cd3f70b7cab8d5064c92b7525652927208349a5
Majid <majid.mjh@gmail.com>
2024-02-24 00:38:40 +0100

    event emitter is moved to service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { EventEmitter2 } from "@nestjs/event-emitter";
-import { LoggerLoggedEvent } from "./events/logger-log.event";
-import { events } from "./constans/events/event.constans";
 
 @Controller()
 export class AppController {
-  constructor(
-    private eventEmitter: EventEmitter2,
-    private readonly appService: AppService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    this.eventEmitter.emit(
-      events.LOGGER_LOG,
-      new LoggerLoggedEvent("Hello dear", Date.now()),
-    );
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { events } from "./constans/events/event.constans";
+import { LoggerLoggedEvent } from "./events/logger-log.event";
 
 @Injectable()
 export class AppService {
+  constructor(private eventEmitter: EventEmitter2) {}
+
   getHello(): string {
+    this.eventEmitter.emit(
+      events.LOGGER_LOG,
+      new LoggerLoggedEvent("Hello dear", Date.now())
+    );
+
     return "Hello World!";
   }
 }
