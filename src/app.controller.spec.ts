968054c5b36a9836306e618ec544592368522ee6
Majid <majid.mjh@gmail.com>
2024-02-23 15:40:48 +0100

    mock EventEmitter in home controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { EventEmitter2 } from "@nestjs/event-emitter";
 
 describe("AppController", () => {
   let appController: AppController;
+  const mockEventEmitter = {
+    emit: jest.fn()
+  };
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+      providers: [AppService, EventEmitter2],
+    })
+    .overrideProvider(EventEmitter2)
+    .useValue(mockEventEmitter)
+    .compile();
 
     appController = app.get<AppController>(AppController);
   });
